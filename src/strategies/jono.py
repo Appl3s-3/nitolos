2f85227a7dcee5918af1410766b3d98bdc4cf049
Appl3s-3 <johnathanmin8@gmail.com>
2025-03-19 21:47:06 +1100

    b

--- src/strategies/jono.py
+++ src/strategies/jono.py
@@ -1,58 +1,56 @@
 import pandas as pd
 
-def buy(close: pd.Series) -> list:
-    buys = []
+# def buy(close: pd.Series) -> list:
+#     buys = []
 
-    sixty_day = []
-    sixty_day_high = 0
-    last_bought = 0
+#     sixty_day = []
+#     sixty_day_high = 0
+#     last_bought = 0
     
-    i = 1
-    end = close.size
-    while i < end:
-        # Maintain sixty_day buffer
-        if len(sixty_day) > 30:
-            sixty_day.pop(0)
-        sixty_day.append(close.iloc[i])
-        sixty_day_high = max(sixty_day)
+#     i = 1
+#     end = close.size
+#     while i < end:
+#         # Maintain sixty_day buffer
+#         if len(sixty_day) > 30:
+#             sixty_day.pop(0)
+#         sixty_day.append(close.iloc[i])
+#         sixty_day_high = max(sixty_day)
 
-        # 60 day high
-        if (close.iloc[i] == sixty_day_high):
-            # Last time bought was over 20 days ago
-            if (i - last_bought) > 20:
-                buys.append(close.index[i])
-                last_bought = i
+#         # 60 day high
+#         if (close.iloc[i] == sixty_day_high):
+#             # Last time bought was over 20 days ago
+#             if (i - last_bought) > 20:
+#                 buys.append(close.index[i])
+#                 last_bought = i
 
-        i += 1
+#         i += 1
 
-    return buys
+#     return buys
 
-def sell(buys: list,
-         close: pd.Series) -> list:
-    sells = []
+# def sell(buys: list,
+#          close: pd.Series) -> list:
+#     sells = []
 
-    sixty_day = []
-    sixty_day_high = 0
-    last_bought = 0
+#     sixty_day = []
+#     sixty_day_high = 0
+#     last_bought = 0
 
-    i = 0
-    end = close.size
-    while i < end:
-        # Maintain sixty_day buffer
-        if len(sixty_day) > 300:
-            sixty_day.pop(0)
-        sixty_day.append(close.iloc[i])
-        sixty_day_high = max(sixty_day)
+#     i = 0
+#     end = close.size
+#     while i < end:
+#         # Maintain sixty_day buffer
+#         if len(sixty_day) > 300:
+#             sixty_day.pop(0)
+#         sixty_day.append(close.iloc[i])
+#         sixty_day_high = max(sixty_day)
 
-        # 60 day high
-        if (close.iloc[i] == sixty_day_high):
-            # Last time bought was over 20 days ago
-            if (i - last_bought) > 20:
-                sells.append(close.index[i])
-                last_bought = i
+#         # 60 day high
+#         if (close.iloc[i] == sixty_day_high):
+#             # Last time bought was over 20 days ago
+#             if (i - last_bought) > 20:
+#                 sells.append(close.index[i])
+#                 last_bought = i
 
-        i += 1
+#         i += 1
 
-    return sells
-
-
+#     return sells
